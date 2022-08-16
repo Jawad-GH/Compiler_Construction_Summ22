ab1c9a7702161b467bd472d4817a25f84999a85c
r4f4y <74683208+r4f4y@users.noreply.github.com>
2022-08-16 13:23:57 +0500

    Update switch.c

--- Project/Phase 3/Code/switch.c
+++ Project/Phase 3/Code/switch.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main()
+{
+    int inp;
+
+    inp = 2;
+
+    switch ( inp )
+    {
+         case 1 : 
+            printf( "\Rafay" );
+            break;
+         case 2 : 
+            printf( "\Jawad" );
+            break;
+         
+        default : 
+            printf( "\CC");
+    }
+
+    return 0;
+}
