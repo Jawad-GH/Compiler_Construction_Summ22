f80250f6e19e40c9a84049b9558370b0d36f18cc
r4f4y <74683208+r4f4y@users.noreply.github.com>
2022-08-16 13:26:35 +0500

    Update swout.py

--- Project/Phase 3/Output/swout.py
+++ Project/Phase 3/Output/swout.py
@@ -1,1 +1,17 @@
-
+def main():
+	
+	int = 2
+	match int:
+		case 1:
+			print("\Rafay")
+      break:
+			
+		case 2:
+			print("\Jawad")
+      break:
+			
+		case 3:
+	 		print("\CompilerConstruction")
+    break:
+		
+	return 0
